041c5d78b9905b7cd2d5c449d32cbc5afeca3591
tuanba <tuanbui.10103@gmail.com>
2025-04-01 16:58:04 +0700

    feat: add FE

---
+++ client/src/components/Navbars/AdminNavbar.js
@@ -0,0 +1,95 @@
+import { Link } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Form,
+  FormGroup,
+  InputGroupText,
+  Input,
+  InputGroup,
+  Navbar,
+  Nav,
+  Container,
+  Media,
+} from "reactstrap";
+
+const AdminNavbar = (props) => {
+  return (
+    <>
+      <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
+        <Container fluid>
+          <Link
+            className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
+            to="/"
+          >
+            {props.brandText}
+          </Link>
+          {/* Search Bar */}
+          <Form className="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
+            <FormGroup className="mb-0">
+              <InputGroup className="input-group-alternative">
+                <InputGroupText>
+                  <i className="fas fa-search" />
+                </InputGroupText>
+                <Input placeholder="Search" type="text" />
+              </InputGroup>
+            </FormGroup>
+          </Form>
+          {/* User Dropdown */}
+          <Nav className="align-items-center d-none d-md-flex" navbar>
+            <UncontrolledDropdown nav>
+              <DropdownToggle className="pr-0" nav>
+                <Media className="align-items-center">
+                  <span className="avatar avatar-sm rounded-circle">
+                    <img
+                      alt="User"
+                      src={require("../../assets/img/theme/team-4-800x800.jpg")}
+                    />
+                  </span>
+                  <Media className="ml-2 d-none d-lg-block">
+                    <span className="mb-0 text-sm font-weight-bold">
+                      Jessica Jones
+                    </span>
+                  </Media>
+                </Media>
+              </DropdownToggle>
+              <DropdownMenu className="dropdown-menu-arrow" right>
+                <DropdownItem className="noti-title" header tag="div">
+                  <h6 className="text-overflow m-0">Welcome!</h6>
+                </DropdownItem>
+                <DropdownItem to="/admin/user-profile" tag={Link}>
+                  <i className="ni ni-single-02" />
+                  <span>My Profile</span>
+                </DropdownItem>
+                <DropdownItem to="/admin/settings" tag={Link}>
+                  <i className="ni ni-settings-gear-65" />
+                  <span>Settings</span>
+                </DropdownItem>
+                <DropdownItem to="/admin/activity" tag={Link}>
+                  <i className="ni ni-calendar-grid-58" />
+                  <span>Activity</span>
+                </DropdownItem>
+                <DropdownItem to="/admin/support" tag={Link}>
+                  <i className="ni ni-support-16" />
+                  <span>Support</span>
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem
+                  href="#logout"
+                  onClick={(e) => e.preventDefault()}
+                >
+                  <i className="ni ni-user-run" />
+                  <span>Logout</span>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default AdminNavbar;
---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,246 @@
+import { useState } from "react";
+import { NavLink as NavLinkRRD, Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Collapse,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupText,
+  InputGroup,
+  Media,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+const Sidebar = (props) => {
+  const [collapseOpen, setCollapseOpen] = useState(false);
+
+  const activeRoute = (routeName) => {
+    return props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  };
+
+  const toggleCollapse = () => {
+    setCollapseOpen(!collapseOpen);
+  };
+
+  const closeCollapse = () => {
+    setCollapseOpen(false);
+  };
+
+  const createLinks = (routes) => {
+    return routes.map((prop, key) => (
+      <NavItem key={key}>
+        <NavLink
+          to={prop.layout + prop.path}
+          tag={NavLinkRRD}
+          onClick={closeCollapse}
+        >
+          <i className={prop.icon} />
+          {prop.name}
+        </NavLink>
+      </NavItem>
+    ));
+  };
+
+  const { bgColor, routes, logo } = props;
+  let navbarBrandProps = {};
+
+  if (logo?.innerLink) {
+    navbarBrandProps = { to: logo.innerLink, tag: Link };
+  } else if (logo?.outterLink) {
+    navbarBrandProps = { href: logo.outterLink, target: "_blank" };
+  }
+
+  return (
+    <Navbar
+      className="navbar-vertical fixed-left navbar-light bg-white"
+      expand="md"
+      id="sidenav-main"
+    >
+      <Container fluid>
+        {/* Toggler */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleCollapse}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        {/* Brand */}
+        {logo && (
+          <NavbarBrand className="pt-0" {...navbarBrandProps}>
+            <img
+              alt={logo.imgAlt}
+              className="navbar-brand-img"
+              src={logo.imgSrc}
+            />
+          </NavbarBrand>
+        )}
+
+        {/* User Menu */}
+        <Nav className="align-items-center d-md-none">
+          <UncontrolledDropdown nav>
+            <DropdownToggle nav className="nav-link-icon">
+              <i className="ni ni-bell-55" />
+            </DropdownToggle>
+            <DropdownMenu className="dropdown-menu-arrow" right>
+              <DropdownItem>Action</DropdownItem>
+              <DropdownItem>Another action</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Something else here</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+          <UncontrolledDropdown nav>
+            <DropdownToggle nav>
+              <Media className="align-items-center">
+                <span className="avatar avatar-sm rounded-circle">
+                  <img
+                    alt="User"
+                    src={require("../../assets/img/theme/team-1-800x800.jpg")}
+                  />
+                </span>
+              </Media>
+            </DropdownToggle>
+            <DropdownMenu className="dropdown-menu-arrow" right>
+              <DropdownItem className="noti-title" header tag="div">
+                <h6 className="text-overflow m-0">Welcome!</h6>
+              </DropdownItem>
+              <DropdownItem to="/admin/user-profile" tag={Link}>
+                <i className="ni ni-single-02" />
+                <span>My Profile</span>
+              </DropdownItem>
+              <DropdownItem to="/admin/settings" tag={Link}>
+                <i className="ni ni-settings-gear-65" />
+                <span>Settings</span>
+              </DropdownItem>
+              <DropdownItem to="/admin/activity" tag={Link}>
+                <i className="ni ni-calendar-grid-58" />
+                <span>Activity</span>
+              </DropdownItem>
+              <DropdownItem to="/admin/support" tag={Link}>
+                <i className="ni ni-support-16" />
+                <span>Support</span>
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem href="#logout" onClick={(e) => e.preventDefault()}>
+                <i className="ni ni-user-run" />
+                <span>Logout</span>
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+
+        {/* Sidebar Menu */}
+        <Collapse navbar isOpen={collapseOpen}>
+          {/* Collapse header */}
+          <div className="navbar-collapse-header d-md-none">
+            <Row>
+              {logo && (
+                <Col className="collapse-brand" xs="6">
+                  {logo.innerLink ? (
+                    <Link to={logo.innerLink}>
+                      <img alt={logo.imgAlt} src={logo.imgSrc} />
+                    </Link>
+                  ) : (
+                    <a href={logo.outterLink}>
+                      <img alt={logo.imgAlt} src={logo.imgSrc} />
+                    </a>
+                  )}
+                </Col>
+              )}
+              <Col className="collapse-close" xs="6">
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  onClick={toggleCollapse}
+                >
+                  <span />
+                  <span />
+                </button>
+              </Col>
+            </Row>
+          </div>
+
+          {/* Search Form */}
+          <Form className="mt-4 mb-3 d-md-none">
+            <InputGroup className="input-group-rounded input-group-merge">
+              <Input
+                aria-label="Search"
+                className="form-control-rounded form-control-prepended"
+                placeholder="Search"
+                type="search"
+              />
+              <InputGroupText>
+                <span className="fa fa-search" />
+              </InputGroupText>
+            </InputGroup>
+          </Form>
+
+          {/* Navigation Links */}
+          <Nav navbar>{createLinks(routes)}</Nav>
+
+          {/* Divider */}
+          <hr className="my-3" />
+
+          {/* Documentation */}
+          <h6 className="navbar-heading text-muted">Documentation</h6>
+          <Nav className="mb-md-3" navbar>
+            <NavItem>
+              <NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/overview?ref=adr-admin-sidebar">
+                <i className="ni ni-spaceship" />
+                Getting Started
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/colors?ref=adr-admin-sidebar">
+                <i className="ni ni-palette" />
+                Foundation
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://demos.creative-tim.com/argon-dashboard-react/#/documentation/alerts?ref=adr-admin-sidebar">
+                <i className="ni ni-ui-04" />
+                Components
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+Sidebar.defaultProps = {
+  routes: [],
+};
+
+Sidebar.propTypes = {
+  routes: PropTypes.arrayOf(PropTypes.object),
+  logo: PropTypes.shape({
+    innerLink: PropTypes.string,
+    outterLink: PropTypes.string,
+    imgSrc: PropTypes.string.isRequired,
+    imgAlt: PropTypes.string.isRequired,
+  }),
+};
+
+export default Sidebar;
